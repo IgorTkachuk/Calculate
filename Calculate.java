ffc23fef19db7d48f4a939014a2b05fcb2b8eb36
ITka4yk <I.Tka4yk@gmail.com>
2017-02-12 11:13:38 +0200

    First commit

---
+++ Calculate.java
@@ -0,0 +1,11 @@
+class Calculate {
+
+  public static void main(String[] args) {
+    int parameter1 = Integer.valueOf(args[0]);
+    int parameter2 = Integer.valueOf(args[1]);
+    int sum = parameter1 + parameter2;
+
+      System.out.println("Sum = " + sum);
+  }
+
+}
